5b4aa493ee65aeb4788c932a1fb0395cfdf14f13
por292 <134024220+por292@users.noreply.github.com>
2026-08-11 13:44:54 +1000

    Update Clanker1.ino

--- Clanker1.ino
+++ Clanker1.ino
@@ -1,9 +1,41 @@
+#include <Servo.h>
+
+Servo myservo;  // create servo object to control a servo
+Servo myservo1;
+Servo myservo2;
+
+
 void setup() {
-  // put your setup code here, to run once:
+
+  myservo.attach(4);
+  myservo1.attach(5);
+  myservo2.attach(6);
+  Serial.begin(9600);
 
 }
 
+
+
 void loop() {
-  // put your main code here, to run repeatedly:
+ Claw();
+
+}
+
+void Claw(){
+
+  myservo.write(0); 
+  myservo1.write(0);
+  myservo2.write(0);
+  Serial.println(myservo.read());
+  Serial.println(myservo1.read());
+  Serial.println(myservo2.read());
+  //delay(500);
+  myservo.write(110);
+  myservo1.write(110);
+  myservo2.write(110);
+  Serial.println(myservo.read());
+  Serial.println(myservo1.read());
+  Serial.println(myservo2.read());
+  //delay(500);
 
 }
