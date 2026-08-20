337d8276faf0d409c5a2e6607aaf7cdc879ae139
por292 <134024220+por292@users.noreply.github.com>
2026-08-20 10:13:21 +1000

    Update Clanker1.ino

--- Clanker1.ino
+++ Clanker1.ino
@@ -5,6 +5,12 @@ Servo myservo;  // create servo object to control a servo
 Servo myservo1;
 QTRSensors qtr;
 
+const int echoPin = (12);
+const int trigPin = (11);
+int duration, distance;
+unsigned long c_time = millis();
+unsigned long p_time;
+
 const uint8_t SensorCount = 4;
 uint16_t sensorValues[SensorCount];
 
@@ -14,39 +20,43 @@ void setup() {
   myservo1.attach(10);
   qtr.setTypeAnalog();
 
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+
   qtr.setSensorPins((const uint8_t[]){A0, A1, A2, A3}, SensorCount);
 
-  
   Serial.begin(9600);
 
 }
 
-
-
-void loop() {
+void loop() { 
  Claw();
-
 }
 
 void Claw(){
-
-  myservo.write(180); 
+  if(millis() - p_time > 250){ //Custom delay so everthing can work in sync
+  p_time = millis();
+  myservo.write(180);
   myservo1.write(0);
- 
-  Serial.println(myservo.read());
-  Serial.println(myservo1.read());
-  
-  delay(500);
-  
+  }
+
+  if(millis() - p_time > 250){
+  p_time = millis();
   myservo.write(95);
   myservo1.write(88);
+  }
 
-  Serial.println(myservo.read());
-  Serial.println(myservo1.read());
-  
-  
-  delay(500);
-
+  if(millis() - p_time > 250){
+  p_time = millis();
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  long duration = pulseIn(echoPin, HIGH);
+  int distance = duration * 0.034 / 2;
+  Serial.println(distance);
+  }
 }
 
 void Line(){
