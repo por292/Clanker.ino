06b6fb346c1c43f1a90c547a6f35f9c3402bfece
por292 <134024220+por292@users.noreply.github.com>
2026-08-25 13:43:04 +1000

    Update Clanker1.ino

--- Clanker1.ino
+++ Clanker1.ino
@@ -3,27 +3,39 @@
 
 Servo myservo;  // create servo object to control a servo
 Servo myservo1;
-QTRSensors qtr;
+QTRSensors qtrA;
 
 const int echoPin = (12);
 const int trigPin = (11);
 int duration, distance;
-unsigned long c_time = millis();
+unsigned long c_time;// = millis();
 unsigned long p_time;
+unsigned long t_time;
 
 const uint8_t SensorCount = 4;
 uint16_t sensorValues[SensorCount];
 
 void setup() {
 
+  for (uint16_t i = 0; i < 400; i++) // calabrate by moving accros a dark line
+  {
+    qtrA.calibrate();
+  }
+  for (uint8_t i = 0; i < SensorCount; i++)
+  {
+    Serial.print(qtrA.calibrationOn.maximum[i]);
+
+    Serial.print(' ');
+  }
   myservo.attach(9);
   myservo1.attach(10);
-  qtr.setTypeAnalog();
+  //qtr.setTypeAnalog();
+  qtrA.setTypeRC();
 
   pinMode(trigPin, OUTPUT);
   pinMode(echoPin, INPUT);
 
-  qtr.setSensorPins((const uint8_t[]){A0, A1, A2, A3}, SensorCount);
+  qtrA.setSensorPins((const uint8_t[]){A0, A1, A2, A3}, SensorCount);
 
   Serial.begin(9600);
 
@@ -32,20 +44,21 @@ void setup() {
 void loop() { 
  Claw();
  Line();
+ Clench();
 }
 
 void Claw(){
-  if(millis() - p_time > 250){ //Custom delay so everthing can work in sync
+ /* if(millis() - p_time > 250){ //Custom delay so everthing can work in sync
   p_time = millis();
   myservo.write(180);
   myservo1.write(0);
-  }
+  }*/
 
-  if(millis() - p_time > 250){
+  /*if(millis() - p_time > 250){
   p_time = millis();
   myservo.write(95);
   myservo1.write(88);
-  }
+  }*/
 
   if(millis() - p_time > 250){
   p_time = millis();
@@ -62,7 +75,11 @@ void Claw(){
 
 void Line(){
 
- qtr.read(sensorValues);
+if (millis() - t_time > 250){
+  t_time = millis();
+  qtrA.read(sensorValues);
+}
+ //qtr.read(sensorValues);
   // print the sensor values as numbers from 0 to 1023, where 0 means maximum
   // reflectance and 1023 means minimum reflectance
   for (uint8_t i = 0; i < SensorCount; i++)
@@ -77,22 +94,21 @@ void Line(){
 }
 void Clench(){
 
-  if (distance == 5){
+  if (distance <= 7){
     
-    if(millis() - p_time > 250){
-    p_time = millis();
-    myservo.write(95);
-    myservo1.write(88);
+    if(millis() - c_time > 250){
+    c_time = millis();
+    myservo.write(180);
+    myservo1.write(0);
     }
   }
-  else
+  else if (distance >=7)
   {
   
-   if(millis() - p_time > 250){
-   p_time = millis();
-   myservo.write(180);
-   myservo1.write(0);
+   if(millis() - c_time > 250){
+   c_time = millis();
+   myservo.write(90);
+   myservo1.write(80);
     }
   }
 }
-
